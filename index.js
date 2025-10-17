b2705188ab54b63d1c0ca6b43e91d988199b0959
DeveloperManishR <manishrawat0711@gmail.com>
2025-10-17 07:33:11 +0530

    Revert "code strucre of react"
    This reverts commit e63c9bbb7f7928ce211ac8879c5b1ff32dab89ad.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
-    <div>index1</div>
+    <View>
+      <Text>index</Text>
+    </View>
   )
 }
 
